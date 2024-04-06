d56fa753db463cc629bdf5ec76a96d7e1ea1c2f6
AbdUlHameed Murie <hmidmrii@gmail.com>
2024-04-06 21:33:46 +0400

    fix: typo

--- src/helpers/bundle-bin-if-needed.ts
+++ src/helpers/bundle-bin-if-needed.ts
@@ -12,8 +12,6 @@ import type { OptimalPkgModel } from '$models/optimal-pkg';
 import type { OptionsModel } from '$models/options';
 import { jsExtensions } from '$constants/js-extensions';
 import type { ContextModel } from '$models/context';
-import { isTs } from '$utils/is-ts';
-import { isJsx } from '$utils/is-jsx';
 
 export const bundleBinIfNeeded = async (
   options: OptionsModel,
@@ -21,9 +19,6 @@ export const bundleBinIfNeeded = async (
   ctx: ContextModel
 ) => {
   if (!pkg['bin:source'] || !pkg.bin) return;
-
-  const ts = isTs(pkg['bin:source']);
-  const jsx = isJsx(pkg['bin:source']);
 
   const bundle = await rollup({
     input: path.resolve(options.cwd, pkg['bin:source']),
