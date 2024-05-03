4e49ed723091dd624e0b5f5f8b4c568427c8291d
AbdUlHameed Murie <hmidmrii@gmail.com>
2024-05-03 17:53:29 +0400

    fix: test ci

--- src/program.ts
+++ src/program.ts
@@ -6,5 +6,4 @@ export const prog = new Command(pkg.name);
 prog
   .version(pkg.version)
   .option('-c, --cwd <cwd>', 'root dir path of your lib', '.')
-  // .option('-s, --single', 'do you want to build to single file?', false)
   .parse();
